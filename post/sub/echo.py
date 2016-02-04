c6192574a935e81fc1fb39fb293cc34702a9f88b
J-Adrian-Zimmer <payL@jazimmer.net>
2016-02-04 18:55:35 -0500

    Cleaned up again.  The install files are like example3's at a time when example2 hasn't been written.

--- post/sub/echo.py
+++
@@ -1,9 +0,0 @@
-def respond( env, req_obj, send_response ):
-     print "ECHOING"
-     send_response(
-         "ok",
-         "<h1>Your message was:</h1><p>" +
-         req_obj.payload.upper() +
-         "</p>"
-     ) 
-
