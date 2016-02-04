b6388cbc556fb6b74a4a66e3cd0f89da8181bf8b
J-Adrian-Zimmer <payL@jazimmer.net>
2016-02-04 18:37:59 -0500

    Cleaned this up so it is the example3 branch that only imports responders

--- install/serveYOS.py
+++ install/serveYOS.py
@@ -53,70 +53,57 @@ except:
 ## helper functions 
 ##   These require a handler instance.
 
-def make_send_response( handler ):
-  def send_response(response,payload): 
-     send_me = unicode( json.dumps( 
-         { "response": response, 
-           "payload": payload,
-           "session": ""
-         }
-     ), 'utf-8')
-     handler.send_response(200)
-     handler.send_header( 
-         "Content-Type", 
-         "text/plain; charset=utf-8" 
-     )
-     handler.send_header(
-          'content-length',
-          str(len(send_me))
-     )
-     handler.end_headers()
-     handler.wfile.write(send_me)
-  return send_response
+
+def send_response( handler, response, payload ):
+  send_me = unicode( json.dumps( 
+      { "response": response, 
+        "payload": payload,
+        "session": ""
+      }
+  ), 'utf-8')
+  handler.send_response(200)
+  handler.send_header( 
+      "Content-Type", 
+      "text/plain; charset=utf-8" 
+  )
+  handler.send_header(
+       'content-length',
+       str(len(send_me))
+  )
+  handler.end_headers()
+  handler.wfile.write(send_me)
+
 
 def error(handler,msg):
-  make_send_response(handler)( 
-           "error", 
-           "ERROR: " + msg 
-  )
+  send_response(handler, "error", "ERROR: " + msg )
    
+
 def respond_to_post_request(handler,cmd):
-   
-   try:                           # get the desired .py #
-      with open(cmd+'.py') as fi: 
-          exec_me = fi.read()
+  
+   try:  # get the desired .py
+      py = __import__(cmd)
    except:
       error(handler, "No %s command can be found" %  cmd )
 
-   try:              # get respond function from exec_me #
-      exec(exec_me)
-   except:
-      error(handler, "Difficulty with %s.py" % cmd)
-
-   try:            # get the length of the incoming json #
+   try:  # get the length of the incoming json
      conlen = handler.headers['content-length']
    except:
      conlen = 0
-
-   try:              # make incoming json into an object #
-     obj =  _Bunch(
-               json.loads(
-                  handler.rfile.read(int(conlen))
-            )  )
-     print "incoming payload:" + obj.payload
+    
+   try:  # evaluate and send the response
+      obj = _Bunch(json.loads(handler.rfile.read(int(conlen))))
    except:
-     error( handler, "Cannot process incoming json" )
-       
-   try:                 # evaluate and send the response #
-      respond( 
-         env,
+      error(handler, "Cannot process incoming json")
+
+   try:
+      py.respond( 
+         env, 
          obj,
-         make_send_response(handler)
+         lambda r,c: send_response(handler,r,c)
       )
    except:
       error(handler,"Cannot process response.")
-    
-     
+      
 
 
 ## extend SimpleHTTPRequestHandler for POST requests ##
@@ -124,14 +111,23 @@ def respond_to_post_request(handler,cmd):
 class PostHandler (SimpleHTTPRequestHandler):
 
     def do_POST(self):
-        os.chdir( '../post' )
         scheme,netloc,path,params,query,fragment = \
                          urlparse.urlparse(self.path)
         cmd = path[1:] 
         env['THE_POST_COMMAND'] = cmd 
+
+        # get cmd's directory in the sys.path
+        dircwd = op.dirname(os.getcwd())
+        dircmd,cmd = op.split(cmd)
+        if dircmd:
+           sys.path.append( op.join(dircwd, 'post', dircmd) )
+           print 'APPENDED:' + op.join(dircwd, 'post', dircmd)
+        else: 
+           sys.path.append( op.join(dircwd, 'post') )
+
+                
         respond_to_post_request(self, cmd )
-        print 'RESPONDED'
-        os.chdir('../get')
+        
 
 
 
